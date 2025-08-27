03b05ef5f05c9bf404a37a550e47f97f784f9055
CJKorn <cjkornie@gmail.com>
2025-08-27 19:59:38 +1000

    fix: update maplibre-gl to version 5.7.0 and add CSS to ensure proper rendering of markers

--- src/app/_components/homemap.tsx
+++ src/app/_components/homemap.tsx
@@ -1,13 +1,21 @@
 "use client";
-import Map from 'react-map-gl/maplibre';
+import Map, { Marker } from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css' // Required CSS for MapLibre GL to render marker positions correctly
 
 export default function HomeMap() {
     return <Map
+            style={{ width: '50vw', height: '50vh'}}
             initialViewState={{
-              longitude: -100,
-              latitude: 40,
-              zoom: 3.5
+              longitude: 151.19930,
+              latitude: -33.883993,
+              zoom: 10
             }}
             mapStyle="https://api.maptiler.com/maps/streets/style.json?key=Y1LHHXeWTC4l0lTXoIC4"
-          />;;
+        >
+          <Marker
+              longitude={151.19930}
+              latitude={-33.883993}
+          />
+        </Map>
+    ;
 }

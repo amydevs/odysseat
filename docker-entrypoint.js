afe4ff7219679b1bc1c87123028046f44f6ba1a0
Amy Y <ctrlalternativedelete@gmail.com>
2025-09-02 21:02:51 +1000

    fix: docker-entrypoint

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -6,15 +6,18 @@ const env = { ...process.env }
 
 // If running the web server then prerender pages
 if (process.argv.slice(-2).join(' ') === 'node server.js') {
-  await exec('npx next build --experimental-build-mode generate')
+  await exec('npx --yes next build --experimental-build-mode generate')
 }
 
 // launch application
 await exec(process.argv.slice(2).join(' '))
 
+/**
+ * @param {string} command
+ */
 function exec(command) {
   const child = spawn(command, { shell: true, stdio: 'inherit', env })
-  return new Promise((resolve, reject) => {
+  return /** @type {Promise<void>} */(new Promise((resolve, reject) => {
     child.on('exit', code => {
       if (code === 0) {
         resolve()
@@ -22,5 +25,5 @@ function exec(command) {
         reject(new Error(`${command} failed rc=${code}`))
       }
     })
-  })
+  }))
 }

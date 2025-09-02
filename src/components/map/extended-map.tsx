172390b03772bbbb4b77ecf33bad7af1fa308bd6
Amy Y <ctrlalternativedelete@gmail.com>
2025-09-03 01:05:32 +1000

    fix: extended map now relies on its own context to make sure that rerender is only triggered after ref extension

--- src/components/map/extended-map.tsx
+++ src/components/map/extended-map.tsx
@@ -1,5 +1,6 @@
 import { Map, type LngLat, type MapRef } from "react-map-gl/maplibre";
 import * as React from "react";
+import { ExtendedMapContext } from "~/contexts/extended-map-context";
 
 export type ExtendedMapRef = MapRef & {
     getLastCenter: () => LngLat | undefined;
@@ -10,23 +11,27 @@ export type ExtendedMapProps = React.ComponentProps<typeof Map> & {
 };
 
 export default function ExtendedMap({ref, ...props}: ExtendedMapProps) {
+    const mapRef = React.useRef<ExtendedMapRef>(undefined);
     const lastPos = React.useRef<LngLat>(undefined);
-    return <Map ref={(current) => {
-        if (current == null) {
-            return
-        }
-        Object.assign(current, {
-            ...current,
-            getLastCenter: () => lastPos.current,
-        } satisfies ExtendedMapRef);
-        const moveStartHandler = current.on('movestart', () => {
-            lastPos.current = current.getCenter();
-        });
-        if (ref != null) {
-            (ref as { current: ExtendedMapRef }).current = current as unknown as ExtendedMapRef;
-        }
-        return () => {
-            moveStartHandler.unsubscribe();
-        };
-    }} {...props} />
+    return <ExtendedMapContext.Provider value={mapRef}>
+        <Map ref={(current) => {
+            if (current == null) {
+                return
+            }
+            Object.assign(current, {
+                ...current,
+                getLastCenter: () => lastPos.current,
+            } satisfies ExtendedMapRef);
+            const moveStartHandler = current.on('movestart', () => {
+                lastPos.current = current.getCenter();
+            });
+            mapRef.current = current as unknown as ExtendedMapRef;
+            if (ref != null) {
+                (ref as { current: ExtendedMapRef }).current = current as unknown as ExtendedMapRef;
+            }
+            return () => {
+                moveStartHandler.unsubscribe();
+            };
+        }} {...props} />
+    </ExtendedMapContext.Provider>
 }
--- src/hooks/use-extended-map.ts
+++ src/hooks/use-extended-map.ts
@@ -1,6 +1,9 @@
-import { useMap } from "react-map-gl/maplibre"
-import type { ExtendedMapRef } from "~/components/map/extended-map";
+import * as React from "react";
+import { ExtendedMapContext } from "~/contexts/extended-map-context";
 
 export function useExtendedMap() {
-    return useMap() as ReturnType<typeof useMap> & { current?: ExtendedMapRef };
+    const map = React.useContext(ExtendedMapContext);
+    return map;
+    // console.log(map.current, map.current?.getLastCenter);
+    // return useMap() as ReturnType<typeof useMap> & { current?: ExtendedMapRef };
 }

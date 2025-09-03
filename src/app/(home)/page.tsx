b36dd825c13203417673f5345f9430ea20fd84e2
Amy Y <ctrlalternativedelete@gmail.com>
2025-09-03 19:12:45 +1000

    feat: updated schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/components/post";
 import { api, HydrateClient } from "~/trpc/server";
 import HomeMap from "./map";
 
--- src/server/db/schema/schema.ts
+++ src/server/db/schema/schema.ts
@@ -8,20 +8,6 @@ import {
 import { createTable } from "./utils";
 import { user } from "./auth-schema";
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
-);
-
 export const recipe = createTable(
   "recipe",
   (d) => ({
@@ -42,6 +28,28 @@ export const recipe = createTable(
   (t) => [
     // index("position_idx").on(t.position),
     index("title_idx").on(t.title),
+    // index("rating_idx").on(t.rating),
+    index("created_at_idx").on(t.createdAt),
+  ],
+);
+
+export const comment = createTable(
+  "comment",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    userId: d.text().notNull().references(() => user.id),
+    recipeId: d.integer().notNull().references(() => recipe.id),
+    content: d.text().notNull(),
+    createdAt: d.timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`) // Could be changed to new Date() to be handled by JS but shouldn't matter
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`) // Setting CURRENT_TIMESTAMP as default so we can sort by updatedAt for all markers
+      .$onUpdate(() => sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  }),
+  (t) => [
+    // index("position_idx").on(t.position),
     // index("rating_idx").on(t.rating),
     index("created_at_idx").on(t.createdAt),
   ],

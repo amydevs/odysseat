d79d4c489f2cdc71faff80c1d846ae04dd989171
amy <ctrlalternativedelete@gmail.com>
2025-09-24 00:28:44 +1000

    fix sort orders

--- src/server/db/validators.ts
+++ src/server/db/validators.ts
@@ -2,13 +2,20 @@ import { createSelectSchema, createInsertSchema, createUpdateSchema } from "driz
 import * as z from "zod/v4";
 import { recipe } from "./schema";
 
+export const zSortOptions = z.object({
+    sortBy: z.string().default("id"),
+    sortOrder: z.enum(["asc", "desc"]).default("asc"),
+});
+
 export const zLongLat = z.tuple([z.number(), z.number()]);
 
 export const zRecipeFilter = createSelectSchema(recipe)
     .partial()
+    .and(zSortOptions)
     .and(z.object({
         minPosition: zLongLat.optional(),
         maxPosition: zLongLat.optional(),
+        sortBy: createSelectSchema(recipe).keyof().default("createdAt"),
     }));
 
 export const zRecipeCreate = createInsertSchema(recipe)

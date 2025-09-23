7a9e3d642e7e5c32ac797624d7e5bd3696e89f04
amy <ctrlalternativedelete@gmail.com>
2025-09-23 14:26:07 +1000

    fix: admin user can now login

--- scripts/auth-config.ts
+++ scripts/auth-config.ts
@@ -1,7 +1,8 @@
+import { env } from "~/env";
 import { initAuth } from "~/server/auth";
 
 export const auth = initAuth({
   baseUrl: "",
   productionUrl: "",
-  secret: "",
+  secret: env.AUTH_SECRET,
 });

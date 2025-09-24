d0d05ef9f26ec8307c7e0e7fd88607ef34957909
Ian <142363330+RinreiRay@users.noreply.github.com>
2025-09-24 12:16:58 +1000

    feat: recipes search page

--- src/app/recipes/loading.tsx
+++
@@ -1,5 +0,0 @@
-export default function RecipesLoading() {
-    return <div>
-        hi
-    </div>
-}
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 import { redirect, RedirectType } from "next/navigation";
 import { Button } from "~/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { api } from "~/trpc/server"
 
@@ -17,12 +20,31 @@ export default async function RecipesPage({
         const search = data.get("search");
         redirect(`/recipes?search=${search}`, RedirectType.push);
     }
-    return <main className="max-w-7xl mx-auto">
+    return <main className="max-w-7xl mx-auto space-y-3">
         <form className="flex gap-1">
             <Input defaultValue={search} name="search" />
             <Button formAction={searchAction}>
                 Search
             </Button>
         </form>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-3">
+            {
+                recipes.map((r) => (
+                    <Card key={r.id}>
+                        <CardHeader>
+                            <Image alt={r.title} src={r.thumbnailUrl ?? ""} className="w-full aspect-video object-cover" width={400} height={300}  />
+                            <CardTitle className="text-ellipsis whitespace-nowrap overflow-hidden">{r.title}</CardTitle>
+                        </CardHeader>
+                        <CardFooter>
+                            <Button asChild>
+                                <Link href={`/recipe/${r.id}`}>
+                                    View
+                                </Link>
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))
+            }
+        </div>
     </main>
 }
